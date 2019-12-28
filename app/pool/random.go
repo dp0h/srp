d115773a68fb7f230bd60c1ab339c99aded90eb8
Andrei Visnakovs <andrei.vish@gmail.com>
2019-12-28 00:05:49 +0000

    Enabled weighted host selection

--- app/pool/random.go
+++ app/pool/random.go
@@ -56,12 +56,59 @@ func configToServices(config *config.ConfFile) []*service {
 }
 
 func (p *RandomWeightedPool) Next() (string, error) {
+	alive, err := p.getAlive()
+	if err != nil {
+		return "", err
+	}
+
+	if len(alive) == 1 {
+		return alive[0].Host, nil
+	}
+
+	totalWeight := 0
+	for _, item := range alive {
+		totalWeight += item.Weight
+	}
+
+	r := rand.Intn(totalWeight) + 1
+	for _, item := range alive {
+		r -= item.Weight
+		if r <= 0 {
+			return item.Host, nil
+		}
+	}
+
+	return alive[0].Host, nil
+}
+
+type hostAndWeight struct {
+	Host   string
+	Weight int
+}
+
+func (p *RandomWeightedPool) getAlive() ([]hostAndWeight, error) {
 	p.lock.RLock()
 	defer p.lock.RUnlock()
 
 	if len(p.services) == 0 {
-		return "", errors.New("empty services list")
+		return nil, errors.New("empty services list")
 	}
 
-	return p.services[0].Host, nil
+	var res []hostAndWeight
+
+	for _, svc := range p.services {
+		if svc.alive && svc.Weight > 0 {
+			hw := hostAndWeight{
+				Host:   svc.Host,
+				Weight: svc.Weight,
+			}
+			res = append(res, hw)
+		}
+	}
+
+	if len(res) == 0 {
+		return nil, errors.New("no alive services")
+	}
+
+	return res, nil
 }
--- app/pool/random_test.go
+++ app/pool/random_test.go
@@ -9,7 +9,24 @@ import (
 	"time"
 )
 
-const srpYaml = `
+const srpYaml1 = `
+services:
+  srv1:
+    host: "example1.com"
+    healthcheck: "api/ping"
+    weight: 1
+`
+
+func TestRandomWeightedPool_NextWithOneElement(t *testing.T) {
+	conf := config.NewConf(strings.NewReader(srpYaml1))
+	pl := NewRandomWeightedPool(conf, time.Duration(0), time.Duration(0))
+
+	host, err := pl.Next()
+	require.Nil(t, err)
+	assert.Equal(t, host, "example1.com")
+}
+
+const srpYaml2 = `
 services:
   srv1:
     host: "example1.com"
@@ -19,14 +36,40 @@ services:
   srv2:
     host: "example2.com"
     healthcheck: "api/ping"
+    weight: 1
+
+  srv3:
+    host: "example3.com"
+    healthcheck: "api/ping"
+    weight: 0
+
+  srv4:
+    host: "example4.com"
+    healthcheck: "api/ping"
     weight: 2
 `
 
-func TestRandomWeightedPool_Next(t *testing.T) {
-	conf := config.NewConf(strings.NewReader(srpYaml))
+func TestRandomWeightedPool_NextDistribution(t *testing.T) {
+	conf := config.NewConf(strings.NewReader(srpYaml2))
 	pl := NewRandomWeightedPool(conf, time.Duration(0), time.Duration(0))
 
-	host, err := pl.Next()
-	require.Nil(t, err)
-	assert.Equal(t, host, "example1.com")
+	hosts := make(map[string]int)
+
+	for i := 0; i < 1000; i++ {
+		host, err := pl.Next()
+		require.Nil(t, err)
+
+		hosts[host]++
+	}
+
+	assert.Len(t, hosts, 3)
+
+	assert.Greater(t, hosts["example1.com"], 200)
+	assert.Less(t, hosts["example1.com"], 300)
+
+	assert.Greater(t, hosts["example2.com"], 200)
+	assert.Less(t, hosts["example2.com"], 300)
+
+	assert.Greater(t, hosts["example4.com"], 400)
+	assert.Less(t, hosts["example4.com"], 600)
 }
